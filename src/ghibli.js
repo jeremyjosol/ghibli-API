4b9d1f6eb5cfe184289eaac854a282c6f0b7413e
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-07 09:50:49 -0700

    npm run lint

--- src/ghibli.js
+++ src/ghibli.js
@@ -1,7 +1,7 @@
 export default class StudioGhibli {  
   static async searchGhibli(id) {
     try {
-      const response = await fetch(`https://ghibliapi.vercel.app/films/${id}`);
+    const response = await fetch(`https://ghibliapi.vercel.app/films/${id}`);
       const ghibliResponse = response.json();
       
       if (!response.ok) {
